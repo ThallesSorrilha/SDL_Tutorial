4b4e83abbbd402a6d6e0d641d6bbe139d91dec19
ThallesSorrilha <thallessorrilha@gmail.com>
2024-06-30 17:46:09 -0300

    Timer adition

--- source/main.cpp
+++ source/main.cpp
@@ -6,11 +6,10 @@
 
 #include "Definitions.h"
 #include "Game.h"
+#include "Timer.h"
 
 int main(int argv, char **args)
 {
-
-    Uint32 frameStart;
     int frameTime;
 
     Game::init("SDL Tutorial", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, defaultWidthBlocks * defaultBlockSize, defaultHeightBlocks * defaultBlockSize, SDL_WINDOW_SHOWN); // 864x648
@@ -18,13 +17,13 @@ int main(int argv, char **args)
     while (Game::isRunning())
     {
 
-        frameStart = SDL_GetTicks();
+        Timer::update();
 
         Game::handleEvents();
         Game::update();
         Game::draw();
 
-        frameTime = SDL_GetTicks() - frameStart;
+        frameTime = Timer::getNewTime() - Timer::getGlobal();
         if (frameTime < defaultFrameDelay)
         {
             SDL_Delay(defaultFrameDelay - frameTime);
